5f7dc8b0718b24967daa15293ecf6828d35aecc3
julenvitoria <julenvitoria@hotmail.es>
2021-07-28 20:14:59 +0200

    commit

--- UpdateEmulators.sh
+++ UpdateEmulators.sh
@@ -2,4 +2,4 @@
 
 echo "Downloading emulators.cfg..."
 sleep 2
-wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBA-Emulators.cfg/master/cm3/emulators.cfg>/opt/retropie/configs/all/emulators.cfg
+wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy-emulators.cfg/main/emulators.cfg>/opt/retropie/configs/all/emulators.cfg
